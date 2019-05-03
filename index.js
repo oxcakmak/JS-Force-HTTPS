d5412568f5e8c4be13ac06b162314abe22a3078e
Osman Cakmak <48910999+oxcakmak@users.noreply.github.com>
2019-05-03 23:25:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+Author: Osman Çakmak
+Skype: oxcakmak
+Email: oxcakmak@hotmail.com
+Website: http://oxcakmak.com/
+Country: Turkey [TR]
+*/
+if(location.protocol != 'https:'){ location.href = 'https:' + window.location.href.substring(window.location.protocol.length);}
